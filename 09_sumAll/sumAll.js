dd3c4711bc7bc654b07a01bf1195fc29e4a93ac2
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-07 18:44:21 +0000

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    const isPositive = num1 < 0 || num2 < 0;
+    const isInt = Number.isInteger(num1) || Number.isInteger(num2);
+
+    if (isPositive) return "ERROR";
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if (num1 > num2) {
+        [num1, num2] = [num2, num1];
+    }
+
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
