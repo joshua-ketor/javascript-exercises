353b496cb0e5d8ac3ff1a9b6835c0d3d7d1152cc
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-07 17:27:11 +0000

    Complete 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...otherArgs) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < otherArgs.length; j++) {
+            if(otherArgs.includes(arr[i])) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
